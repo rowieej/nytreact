6d5d1f1a6b865629fb49b5ef1c16251b8a7a12de
rowieej <rowena.jaico@gmail.com>
2016-09-19 15:39:08 -0400

    more boiler plate

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// Include the Main React Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+// This code here allows us to render our main component (in this case "Main")
+ReactDOM.render(
+
+  <h1>test</h1>,
+  document.getElementById('app')
+)
