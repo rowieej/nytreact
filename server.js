8602db67f7017427b1a783e468e5936ce3a41042
rowieej <rowena.jaico@gmail.com>
2016-10-10 11:52:05 -0400

    needed to add saved route

--- server.js
+++ server.js
@@ -1,33 +1,100 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// Include Server Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 
-// database config
-// ============================================
-// Here we find an appropriate database to connect to, defaulting to localhost if we don't find one.
-var uristring = process.env.MONGODB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/nytsearch';
-mongoose.connect(uristring, function (err, res) {
-  if (err)
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  else
-    console.log ('Succeeded connected to: ' + uristring);
+//Require Schemas
+var Article = require('./server/model.js');
+
+// Create Instance of Express
+var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+// -------------------------------------------------
+
+// MongoDB Configuration configuration
+mongoose.connect('mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
 });
 
 
-app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json())
+// -------------------------------------------------
 
-const main = require('./routes/index.js');
-const api = require('./routes/index.js');
-app.use('/', main);
-app.use('/api', api);
+// Main Route
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, function(){
-  console.log('App listening on PORT ' + PORT);
+// Route to get all saved articles
+app.get('/api/saved', function(req, res) {
+
+  Article.find({})
+    .exec(function(err, doc){
+
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.send(doc);
+      }
+    })
+});
+
+// Route to add an article to saved list
+app.post('/api/saved', function(req, res){
+  var newArticle = new Article(req.body);
+
+  console.log(req.body)
+
+  var title = req.body.title;
+  var date = req.body.date;
+  var url = req.body.url;
+
+  newArticle.save(function(err, doc){
+    if(err){
+      console.log(err);
+    } else {
+      res.send(doc._id);
+    }
+  });
+});
+
+// Route to delete an article from saved list
+app.delete('/api/saved/', function(req, res){
+
+  var url = req.param('url');
+
+  Article.find({"url": url}).remove().exec(function(err, data){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.send("Deleted");
+    }
+  });
+});
+
+
+// -------------------------------------------------
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
